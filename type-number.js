9fe2f836b22b83be1801adc36c7cca836a34e2d2
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-07-12 13:43:14 -0300

    Type Number

---
+++ type-number.js
@@ -0,0 +1,34 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 10;
+const segundoNumero = 2;
+
+// SOMA
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+// Subtracao
+// const operacaoMatematica = primeiroNumero - segundoNumero;
+
+// MULTIPLICACAO
+// const operacaoMatematica = primeiroNumero * segundoNumero;
+
+// DIVISAO
+// const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica)
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .9;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// NaN -> Not A Number (não é um numero)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
