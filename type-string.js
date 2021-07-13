cf5e21575c454acb85fa3f8eeb25a22dfa0e34b1
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-07-13 12:53:16 -0300

    Strings e como funcionam

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Oá, mundo!";
+const texto2 = 'Oá mundo!';
+const senha = "senhaSuperSegura456";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu Nome é ";
+const meuNome = "Kaua";
+
+// concatenacao (+)
+
+console.log(citacao + meuNome)
+
+// Template String OU template literal
+
