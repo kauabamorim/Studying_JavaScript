930cc990582b5a5b34f3ef9120cb066372f4e277
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-07-13 13:13:19 -0300

    Tipos de Booleana

---
+++ type-boolean.js
@@ -0,0 +1,20 @@
+// boolean === Comparar
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = Math.floor(Math.random() * 20);
+const segundoNumero = Math.floor(Math.random() * 20);
+
+console.log(primeiroNumero === segundoNumero); 
+
+const a = 5;
+const b = 5;
+
+console.log(a === b); // true
+
+// Comparacao entre Strings
+
+const x = "alura"
+const y = "a"
+
+console.log(x === y); // false
