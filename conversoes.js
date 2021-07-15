86761aa5e01ae338c394113322b723fe8d5b6a48
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-07-15 01:05:12 -0300

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+// Tipo De Dado
+// Booleanos
+
+// conversao implicita
+// converter um dado em outro
+// exemplos: String para um numero, um numero para string etc...
+
+const numero = 456;
+const numeroString = "456";
+
+// console.log(numero + numeroString)
+
+// Conversao explicita
+// Number()
+// String()
+
+console.log(numero + Number(numeroString))
