421ae89ac1ad32aad9d58f32e32ae6479ca0b36c
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-07-29 21:50:44 -0300

    Expressoes-Funcoes

---
+++ expressoes-funcoes.js
@@ -0,0 +1,14 @@
+// console.log(soma(1, 10));
+
+const soma = function(num1, num2) {
+    return num1 + num2;
+}
+console.log(soma(1, 10));
+
+//Diferenca Principal
+
+console.log(apresentar())
+
+function apresentar(){
+    return "Olá"
+}
