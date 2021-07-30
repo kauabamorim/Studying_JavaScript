b82a087c1e7da38cb2abb1cefe139de942755537
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-07-29 22:02:57 -0300

    Arrow-Funcoes

---
+++ arrow-funcoes.js
@@ -0,0 +1,16 @@
+function apresentar(nome) {
+    return `meu nome é ${nome}`;
+}
+
+const apresentarArrow = nome => `meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2;
+
+// Arrow function com + de 1 linha de in
+
+const somaNumerosMenores =  (num1, num2) => {
+    if (num1 || num2 > 10) {
+        return "somente numeros de 1 a 9"
+    } else {
+        return num1 + num2
+    }
+}
