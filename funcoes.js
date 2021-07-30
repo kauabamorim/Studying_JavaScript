014bec0918d8da39f1eaed461feb3496f04e32fc
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-07-29 21:10:57 -0300

    Funcoes

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO
+
+// 1) declara a função
+// 2) valor de soma()
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+                      
+// executa a função (1 ou + vezes)
+                      
+imprimeTexto("oi");
+imprimeTexto("outro texto");
+
+// três formas de escrever funções
+                      
+function soma(){
+    return 2 + 2;
+}
+                      
+console.log(soma());
